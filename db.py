2fe984983334a478dbc03e868ccf32961c27144a
Andrew Rozs <rozs0001@hz.nl>
2023-04-20 14:46:57 +0200

    completeness database done

--- db.py
+++ db.py
@@ -27,19 +27,39 @@ def insert_new_category(category):
     mycursor.execute(f"INSERT INTO categories (category_name) VALUES ('{category}');")
     
     db.commit()
+    
+def create_country_codes_table():
+    db = connection()
+    mycursor = db.cursor()
+    mycursor.execute("""CREATE TABLE country_codes (
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        country_code VARCHAR(6) NOT NULL UNIQUE,
+        country_name VARCHAR(255)
+    );""")
+    
+def insert_new_country(country):
+    db = connection()
+    mycursor = db.cursor()
+    mycursor.execute(f"INSERT INTO country_codes (country_code) VALUES ('{country}');")
+    
+    db.commit()
 
 def create_base_completeness_table():
     db = connection()
     mycursor = db.cursor()
 
     mycursor.execute("""CREATE TABLE completeness (
-        country_code VARCHAR(6) NOT NULL PRIMARY KEY,
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        country_code_id INT UNSIGNED NOT NULL,
         demand_state VARCHAR(255),
         solar_state VARCHAR(255),
         wind_state VARCHAR(255),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (country_code_id) REFERENCES country_codes(id)
     );""")
+    
+    trigger_to_save_history_of_completeness_before_update()
     
 def create_completeness_history_table():
     db = connection()
@@ -63,8 +83,8 @@ def trigger_to_save_history_of_completeness_before_update():
         CREATE TRIGGER completeness_update_trigger
         BEFORE UPDATE ON completeness
         FOR EACH ROW
-        INSERT INTO completeness_history (country_code, demand_state, solar_state, wind_state)
-        VALUES (OLD.country_code, OLD.demand_state, OLD.solar_state, OLD.wind_state
+        INSERT INTO completeness_history (country_code_id, demand_state, solar_state, wind_state)
+        VALUES (OLD.country_code_id, OLD.demand_state, OLD.solar_state, OLD.wind_state
     );""")
 
     
@@ -115,3 +135,20 @@ def fetch_all_categories():
     db.close()
     
     return categories
+
+if __name__ == '__main__':
+    # countries = ['eu', 'al', 'at', 'ba', 'be', 'bg', 'ch', 'cz', 'de', 'dk', 'ee', 'es', 'fi', 'fr', 'gb', 'gr', 'hr', 'hu', 'isem', 'it', 'xk', 'lt', 'lv', 'me', 'mk', 'nl', 'no', 'pl', 'pt', 'ro', 'rs', 'se', 'si', 'sk']
+    # categories = ['demand', 'solar', 'wind']
+    
+    # create_country_codes_table()
+    # create_category_table()
+    
+    # for country in countries:
+    #     insert_new_country(country)
+    
+    # for category in categories:
+    #     insert_new_category(category)
+        
+    # create_base_completeness_table()
+    # create_completeness_history_table()
+    print('idle')
